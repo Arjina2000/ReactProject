86986137668e8a3de044573a2c1176e06775b644
Your Name <you@example.com>
2024-01-17 09:12:24 +0600

    added another part

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css';
+import Cart from '../Cart/Cart';
+
+// import './Cart.jsx';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -30,8 +33,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
